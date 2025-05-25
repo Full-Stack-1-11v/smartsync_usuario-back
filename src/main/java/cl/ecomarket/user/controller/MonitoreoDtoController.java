17f10435f686b544d085744f442dee651f3188f4
cooper8k <ange.prado@duocuc.cl>
2025-05-25 01:58:24 -0400

    3.7 get metodo

--- src/main/java/cl/ecomarket/user/controller/MonitoreoDtoController.java
+++ src/main/java/cl/ecomarket/user/controller/MonitoreoDtoController.java
@@ -1,26 +1,33 @@
 package cl.ecomarket.user.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.ecomarket.user.dto.MonitoreoDTO;
-import cl.ecomarket.user.dto.MonitoreoUserRequest;
-import cl.ecomarket.user.model.User;
+
 
 
 
 @RestController
 @RequestMapping("/api/v1/monitoreo")
 public class MonitoreoDtoController {
- 
+ private final List<MonitoreoDTO> monitoreos = new ArrayList<>();
     @PostMapping("/enviarMonitoreo")
 public ResponseEntity<String> recibirMonitoreo(@RequestBody MonitoreoDTO monitoreoDTO) {
     // Aquí puedes visualizar o procesar los datos recibidos
     System.out.println(monitoreoDTO.getDescripcion());
     return ResponseEntity.ok("Monitoreo recibido correctamente");
 }
+@GetMapping("/todos")
+    public List<MonitoreoDTO> obtenerTodos() {
+        return monitoreos; // Devuelve todos los monitoreos recibidos
+    }
     
 }
