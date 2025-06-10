ca251bd413105adbded59188ba99ad22cddd354c
cooper8k <ange.prado@duocuc.cl>
2025-06-09 23:17:00 -0400

    clases dto completas

--- src/main/java/cl/ecomarket/user/dto/UserProductoDto.java
+++ src/main/java/cl/ecomarket/user/dto/UserProductoDto.java
@@ -1,36 +1,13 @@
 package cl.ecomarket.user.dto;
 
 import cl.ecomarket.user.model.User;
+import lombok.Data;
 
+@Data
 public class UserProductoDto {
     
     private User user;
     private ProductoDto producto;
-
-    
-    public UserProductoDto() {
-    }
-    public UserProductoDto(User user, ProductoDto producto) {
-        this.user = user;
-        this.producto = producto;
-    }
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
-        this.user = user;
-    }
-    public ProductoDto getProducto() {
-        return producto;
-    }
-    public void setProducto(ProductoDto producto) {
-        this.producto = producto;
-    }
-
-
-
-
-    
 
     
 }
